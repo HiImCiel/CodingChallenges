c540a259217533338d3951a68c9b4976c145be73
CielPollard <pitcher1r!>
2020-10-04 15:51:22 -0400

    cleaned up wordcount, added TODO functionality of getting file in another directory

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,25 @@
 # Will read a file that and perform a word count, as well as a complete list of each word used
 
-# Os needed for input
+# Os needed for input, shutil needed for copying
 import os
+import shutil
 
 
-# Gets absolute file path to the curent directory
+# Choose current directory file or file in another directory
 print("Enter the file name to read \n(you are currently in: \n" + os.getcwd() + "\n directory):")
-filename = input()
-filepath = os.path.join(os.getcwd(), filename)
+print("Or type 'Other Directory' to select a file in a nother directory")
 
-# TODO MAKE OPTION TO SELECT A NEW DIRECTORY
+# Case of file in another directory; copies file to CodingChallenges directory
+od = "Other Directory"
+filename = input()
+if filename == od:
+    print("Please input the absolute path name of the directory, including the file name")
+    otherdirect = input()
+    filepath = shutil.copy(otherdirect, os.getcwd())
+
+# File is already in directory
+else:
+    filepath = os.path.join(os.getcwd(), filename)
 
 # Checks validity of filepath, then puts txt contents int a dictionary
 if os.path.exists(filepath):
