0640bfba4759f178b2e46ffc5fa612ade88ecbe8
CielPollard <pitcher1r!>
2020-11-29 00:50:24 -0500

    removed duplicatefinder

--- walk.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for foldername, subfolders, filenames in os.walk('D:\\Videos&Movies'):
-            for i in filenames:
-                print(os.path.abspath(os.path.join(foldername, i)))
-                continue
-
-
